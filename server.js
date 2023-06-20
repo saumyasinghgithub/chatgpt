6c4f1c63075607ff1e7e894706ad208cea745b95
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-06-20 11:47:39 +0530

    created api key from openAI,installed openAI on my node Instance,used fine tuning doc for setting up openAI API key in my nodeJs project

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const openai = require('openai');
+const openaiInstance = new openai.OpenAI("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");
 const app = express();
 const port = 3003
 app.listen(port,()=>{
